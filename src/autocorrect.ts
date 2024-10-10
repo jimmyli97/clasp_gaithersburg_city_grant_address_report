1697d847035baaf6e489ed32ec1e9e5cd28492bd
Jimmy Li <jimmy.jiajian.li@gmail.com>
2024-10-09 23:03:02 -0400

    formatting

--- src/autocorrect.ts
+++ src/autocorrect.ts
@@ -1,20 +1,23 @@
 export namespace Autocorrect {
-
   function testReadQuery() {
-    let rng = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Needs Autocorrect")?.getRange("A2:B2");
-    console.log(rng?.getFormula())
-  
-    rng = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("DB")?.getRange("A2");
+    let rng = SpreadsheetApp.getActiveSpreadsheet()
+      .getSheetByName("Needs Autocorrect")
+      ?.getRange("A2:B2");
+    console.log(rng?.getFormula());
 
-    console.log(Autocorrect.sum(1, 2))
-    
+    rng = SpreadsheetApp.getActiveSpreadsheet()
+      .getSheetByName("DB")
+      ?.getRange("A2");
+
+    console.log(Autocorrect.sum(1, 2));
   }
-  
-  export function sum(a: number,b: number) {
-    return a+b;
+
+  export function sum(a: number, b: number) {
+    return a + b;
   }
 }
 
 function publicTest() {
-  console.log(Autocorrect.sum(1, 2))
+  let x = 1;
+  console.log(Autocorrect.sum(1, 2));
 }
--- src/test/records_integration_test.ts
+++ src/test/records_integration_test.ts
@@ -1,6 +1,9 @@
 // All imports should be ignored by Apps Script
 import { Autocorrect } from "../autocorrect";
 
+// Apps Script functions show up as unused locally
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function publicTest2() {
+  let x =1
   console.log(Autocorrect.sum(1, 2))
 }
