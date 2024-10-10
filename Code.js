b86518da6b4d915e676dc7ef0686590afe97f2b8
Jimmy Li <jimmy.jiajian.li@gmail.com>
2024-10-09 20:10:35 -0400

    reorganize

--- Code.js
+++
@@ -1,7 +0,0 @@
-function testReadQuery() {
-  rng = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Needs Autocorrect").getRange("A2:B2");
-  console.log(rng.getFormula())
-
-   rng = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("DB").getRange("A2");
-  rng.setValue("test")
-}
